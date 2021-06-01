2ed57d4344972ef84b3d596c7f4ae321a0b5a825
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-01 15:55:58 -0700

    adds methods to endcode and decode authorization tokens in application_controller

--- recipe-box-api/app/controllers/application_controller.rb
+++ recipe-box-api/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(data)
+        JWT.encode(data, 'secret')
+    end
+
+    def decode_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'secret')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
 end
