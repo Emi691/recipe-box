28881b9dad269aad4b5cb8a4191148eb953cdd57
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-01 15:06:36 -0700

    restricts routes to only necessary routes, streamlining routing

--- recipe-box-api/config/routes.rb
+++ recipe-box-api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :ingredients
-  resources :recipes
-  resources :users
+  resources :ingredients, only: [:index, :show, :create, :update, :delete]
+  resources :recipes, only: [:index, :show, :create, :update, :delete]
+  resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
