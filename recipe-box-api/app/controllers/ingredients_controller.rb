66b4e0fd40724f70e1f1c12b3a75634a2ac4fb59
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-02 01:51:41 -0700

    fixes bugs with recipes and ingredients controllers

--- recipe-box-api/app/controllers/ingredients_controller.rb
+++ recipe-box-api/app/controllers/ingredients_controller.rb
@@ -2,41 +2,41 @@ class IngredientsController < ApplicationController
 
     def index
         ingredients = Ingredient.all 
-        render json IngredientSerializer.new(ingredients)
+        render json: IngredientSerializer.new(ingredients)
     end
 
     def show
-        ingredient = Ingredient.find_by(name: ingredient_params.name)
-        render json IngredientSerializer.new(ingredient)
+        ingredient = Ingredient.find_by(id: params[:id])
+        render json: IngredientSerializer.new(ingredient)
     end
 
     def create
         ingredient = Ingredient.create(ingredient_params)
         if ingredient.valid?
-            render json IngredientSerializer.new(ingredient)
+            render json: IngredientSerializer.new(ingredient)
         else
-            render json { message: 'Failed to create ingredient'}
+            render json: { message: 'Failed to create ingredient'}
         end
     end
 
     def update
         ingredient = Ingredient.find_by(id: ingredient_params.id)
         if ingredient.update(ingredient_params)
-            render json IngredientSerializer.new(ingredient)
+            render json: IngredientSerializer.new(ingredient)
         else
-            render json { message: 'Failed to update ingredient'}
+            render json: { message: 'Failed to update ingredient'}
         end  
     end
 
     def delete
         ingredient = Ingredient.find_by(id: ingredient_params.id)
         ingredient.destroy
-        render json { message: 'Ingredient successfuly deleted' }
+        render json: { message: 'Ingredient successfuly deleted' }
     end
 
     private
 
     def ingredient_params
-        params.require(:ingredient).permit(:name, :amount)
+        params.require(:ingredient).permit(:name, :amount, :id)
     end
 end
--- recipe-box-api/app/controllers/recipes_controller.rb
+++ recipe-box-api/app/controllers/recipes_controller.rb
@@ -2,41 +2,41 @@ class RecipesController < ApplicationController
 
     def index
         recipes = Recipe.all 
-        render json RecipeSerializer.new(recipes)
+        render json: RecipeSerializer.new(recipes)
     end
 
     def show
-        recipe = Recipe.find_by(title: recipe_params.title)
-        render json RecipeSerializer.new(recipe)
+        recipe = Recipe.find_by(id: params[:id])
+        render json: RecipeSerializer.new(recipe)
     end
 
     def create
         recipe = Recipe.create(recipe_params)
         if recipe.valid?
-            render json RecipeSerializer.new(recipe)
+            render json: RecipeSerializer.new(recipe)
         else
-            render json { message: 'Failed to create recipe'}
+            render json: { message: 'Failed to create recipe'}
         end
     end
 
     def update
         recipe = Recipe.find_by(id: recipe_params.id)
         if recipe.update(recipe_params)
-            render json RecipeSerializer.new(recipe)
+            render json: RecipeSerializer.new(recipe)
         else
-            render json { message: 'Failed to update recipe'}
+            render json: { message: 'Failed to update recipe'}
         end  
     end
 
     def delete
         recipe = Recipe.find_by(id: recipe_params.id)
         recipe.destroy
-        render json { message: 'Recipe successfuly deleted' }
+        render json: { message: 'Recipe successfuly deleted' }
     end
 
     private
 
     def recipe_params
-        params.require(:recipe).permit(:title, :instructions, :photo)
+        params.require(:recipe).permit(:title, :instructions, :photo, :id)
     end
 end
