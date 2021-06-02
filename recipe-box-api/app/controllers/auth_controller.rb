d5ef15ddfc41b917ef0a67bac252cc0d4cc0d648
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-01 17:04:07 -0700

    adds create action for authController

--- recipe-box-api/app/controllers/auth_controller.rb
+++ recipe-box-api/app/controllers/auth_controller.rb
@@ -1,3 +1,12 @@
 class AuthController < ApplicationController
 
+    def create
+        @user = User.find_by(username: login_params[:username])
+        if @user && @user.authenticate(login_params[:password])
+            token = encode_token({ user_id: @user.id })
+            render json: { user: UserSerialiazer.new(@user), jwt: token }, status: :log_in_successful
+        else
+            render json: { message: 'Your username or password is incorrect' }, status: :log_in_failed
+        end
+    end
 end
