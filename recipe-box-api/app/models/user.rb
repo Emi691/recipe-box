9e22a98dde49bda43d3ded8320d78f7c4694547c
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-02 01:39:50 -0700

    seeds test data

--- recipe-box-api/app/models/user.rb
+++ recipe-box-api/app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :recipes
 
-    validates :username, :email, :password, presence: true
-    validates :username, :email, :password, uniqueness: true 
+    validates :username, :email, presence: true
+    validates :username, :email, uniqueness: true 
 end
