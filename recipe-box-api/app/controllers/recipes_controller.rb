558b9a931f93ed1381c4c15b0ff800c26945d98e
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-01 17:31:51 -0700

    adds create action for recipe controller

--- recipe-box-api/app/controllers/recipes_controller.rb
+++ recipe-box-api/app/controllers/recipes_controller.rb
@@ -9,4 +9,9 @@ class RecipesController < ApplicationController
         recipe = Recipe.find_by(title: recipe_params.title)
         render json RecipeSerializer.new(recipe)
     end
+
+    def create
+        recipe = Recipe.create(recipe_params)
+        render json RecipeSerializer.new(recipe)
+    end
 end
