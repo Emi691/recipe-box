7836254ccadd184b72310e7686d42abb94d57c19
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-01 21:11:02 -0700

    adds show action for ingredient controller

--- recipe-box-api/app/controllers/ingredients_controller.rb
+++ recipe-box-api/app/controllers/ingredients_controller.rb
@@ -4,4 +4,9 @@ class IngredientsController < ApplicationController
         ingredients = Ingredient.all 
         render json IngredientSerializer.new(ingredients)
     end
+
+    def show
+        ingredient = Ingredient.find_by(name: ingredient_params.name)
+        render json IngredientSerializer.new(ingredient)
+    end
 end
