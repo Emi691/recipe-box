a1271ed0cdf4e97fa02701e4ab009c4ebc4d208f
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-01 20:44:13 -0700

    adds validations for users

--- recipe-box-api/app/models/user.rb
+++ recipe-box-api/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :recipes
+
+    validates :username, :email, :password, presence: true
+    validates :username, :email, :password, uniqueness: true 
 end
