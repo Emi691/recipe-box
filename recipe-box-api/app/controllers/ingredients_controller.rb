66b146c208345cf76247282303e51c5dba7fd92d
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-01 21:13:06 -0700

    adds show action for create controller

--- recipe-box-api/app/controllers/ingredients_controller.rb
+++ recipe-box-api/app/controllers/ingredients_controller.rb
@@ -9,4 +9,13 @@ class IngredientsController < ApplicationController
         ingredient = Ingredient.find_by(name: ingredient_params.name)
         render json IngredientSerializer.new(ingredient)
     end
+
+    def create
+        ingredient = Ingredient.create(ingredient_params)
+        if ingredient.valid?
+            render json IngredientSerializer.new(ingredient)
+        else
+            remder json { message: 'Failed to create ingredient'}
+        end
+    end
 end
