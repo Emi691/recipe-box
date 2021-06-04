a0a5263c7a265121dddfcfaf07e88add2941e258
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-03 23:35:12 -0700

    imports Home component to App component

--- recipe-box-app/src/App.js
+++ recipe-box-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home'
 import UsersContainer from './containers/UsersContainer';
 import RecipesContainer from './containers/RecipesContainer';
 import IngredientsContainer from './containers/IngredientsContainer';
@@ -7,6 +8,7 @@ import IngredientsContainer from './containers/IngredientsContainer';
 function App() {
   return (
     <div className="App">
+      <Home />
       <UsersContainer />
       <RecipesContainer />
       <IngredientsContainer />
