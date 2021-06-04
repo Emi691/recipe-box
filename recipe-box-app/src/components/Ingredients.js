6e61e4fb651c9e73f828cf7c5862a4d1ba8c42f1
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-03 17:11:27 -0700

    adds Ingredient component scaffolding

---
+++ recipe-box-app/src/components/Ingredients.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Ingredients extends Component {
+
+    render() {
+        return (
+            <div>
+                <Ingredient />
+            </div>
+        )
+    }
+}
+
+export default Ingredients
