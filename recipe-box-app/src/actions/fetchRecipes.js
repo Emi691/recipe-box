debd1057a7a4705eb05986ad1d272a9619a092bc
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-04 12:18:33 -0700

    adds mapDispatchToProps and mapStateToProps to recipesContainer and updates fetchRecipes action creator accordingly

--- recipe-box-app/src/actions/fetchRecipes.js
+++ recipe-box-app/src/actions/fetchRecipes.js
@@ -1,5 +1,8 @@
 export default function fetchRecipes() {
     return (dispatch) => {
         dispatch({ type: 'LOADING_RECIPES' })
+        fetch('http://localhost3000/recipes')
+            .then(resp => resp.json())
+            .then(recipes => dispatch({ type: 'ADD_RECIPES', recipes }))
     }
 }
