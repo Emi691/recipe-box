b18344e54adcf0d6c43444c64cfefacca602d379
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-04 11:24:03 -0700

    adds simple ingredientReducer

---
+++ recipe-box-app/src/reducers/IngredientReducer.js
@@ -0,0 +1,15 @@
+const ingredientReducer = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_RECIPE":
+            return [...state, action.ingredient]
+
+        case "REMOVE_RECIPE":
+            return state.filter(ingredient => ingredient.id !== action.id)
+        
+        default:
+            return state
+    }
+}
+    
+
+export default ingredientReducer
