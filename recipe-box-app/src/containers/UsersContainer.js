1133c211268382b3b8ff3b1d75fc42fdc2aaed93
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-03 17:20:09 -0700

    imports Signup and Login components to UsersContainer

--- recipe-box-app/src/containers/UsersContainer.js
+++ recipe-box-app/src/containers/UsersContainer.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react' 
+import React, {Component} from 'react'
+import Signup from '../components/Signup' 
+import Login from '../components/Login'
 
 class UsersContainer extends Component {
 
