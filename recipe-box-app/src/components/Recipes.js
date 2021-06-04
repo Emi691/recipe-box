fe0ebd13295da0e2294ef00b9a6f3ade36761e1f
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-03 17:05:51 -0700

    adds Recipes component scaffolding

---
+++ recipe-box-app/src/components/Recipes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Recipes extends Component {
+
+    render() {
+        return (
+            <div>
+                <Recipe />
+            </div>
+        )
+    }
+}
+
+export default Recipes
