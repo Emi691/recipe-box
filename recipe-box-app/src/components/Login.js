fdb7b9b3094722ffcc28ba7125b6a6a1d147c8c4
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-06 00:11:18 -0700

    adds state to login component and uses state to set input values

--- recipe-box-app/src/components/Login.js
+++ recipe-box-app/src/components/Login.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Login = () => (
-    <div>
-        <h3>Log In</h3>
-    <form>
-        <div>
-            <label>Username: </label>
-            <input type="text" />
-        </div><br/>
-        <div>
-            <label>Password: </label>
-            <input type="password" />
-        </div>
-            <input type="submit" value="Log In"/>
-    </form>
-    </div>
-)
+class Login extends Component {
+    state = {
+        username: '',
+        password: '',
+    }
+
+    render(){
+        return(
+           <div>
+            <h3>Log In</h3>
+            <form>
+                <div>
+                    <label>Username: </label>
+                    <input type="text" value={this.state.username}/>
+                </div><br/>
+                <div>
+                    <label>Password: </label>
+                    <input type="password" value={this.state.password}/>
+                </div>
+                <input type="submit" value="Log In"/>
+            </form>
+        </div> 
+        )
+    }
+    
+}
 
 export default Login
