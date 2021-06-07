ef3c390b7e81174d3d7f11ee7b723a518e067d74
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-07 10:35:14 -0700

    adds handleChange and handleSubmit event handlers and component state to recipeInput

--- recipe-box-app/src/components/RecipeInput.js
+++ recipe-box-app/src/components/RecipeInput.js
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const RecipeInput = (props) => (
-    <form>
-        <div>
-            <label>Title: </label>
-            <input type="text" name="title" onChange={event => this.handleChange(event)} value={this.state.title}/>
-        </div><br/>
-        <div>
-            <label>Instructions: </label>
-            <input type="text" name="instructions" onChange={event => this.handleChange(event)} value={this.state.instructions}/>
-        </div><br/>
-        <div>
-            <label>Photo Url: </label>
-            <input type="password" name="photo" onChange={event => this.handleChange(event)} value={this.state.photo}/>
-        </div>
-        <input type="submit" value="Add Recipe"/>    
-    </form>
-)
+class RecipeInput extends Component {
+    state = {
+        title: '',
+        insructions: '',
+        photo: '' 
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.login(this.state) 
+    }
+
+    render() {
+        return (
+            <form>
+                <div>
+                    <label>Title: </label>
+                    <input type="text" name="title" onChange={event => this.handleChange(event)} value={this.state.title}/>
+                </div><br/>
+                <div>
+                    <label>Instructions: </label>
+                    <input type="text" name="instructions" onChange={event => this.handleChange(event)} value={this.state.instructions}/>
+                </div><br/>
+                <div>
+                    <label>Photo Url: </label>
+                    <input type="password" name="photo" onChange={event => this.handleChange(event)} value={this.state.photo}/>
+                </div>
+                <input type="submit" value="Add Recipe"/>    
+            </form>    
+        )
+    }
+}
 
 export default RecipeInput
