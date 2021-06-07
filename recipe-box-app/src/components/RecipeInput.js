d9dbf42890d243ab474093bab3c47f58ce5cf19d
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-07 10:28:43 -0700

    sets up recipe form

--- recipe-box-app/src/components/RecipeInput.js
+++ recipe-box-app/src/components/RecipeInput.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const RecipeInput = () => (
-    <p>RecipeInput</p> 
+const RecipeInput = (props) => (
+    <form>
+        <div>
+            <label>Title: </label>
+            <input type="text" name="title" onChange={event => this.handleChange(event)} value={this.state.title}/>
+        </div><br/>
+        <div>
+            <label>Instructions: </label>
+            <input type="text" name="instructions" onChange={event => this.handleChange(event)} value={this.state.instructions}/>
+        </div><br/>
+        <div>
+            <label>Photo Url: </label>
+            <input type="password" name="photo" onChange={event => this.handleChange(event)} value={this.state.photo}/>
+        </div>
+        <input type="submit" value="Add Recipe"/>    
+    </form>
 )
 
 export default RecipeInput
