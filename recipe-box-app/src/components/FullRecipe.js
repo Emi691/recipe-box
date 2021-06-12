588d4f51c1f15c10d6cd899adfe9d3a865a18fa1
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-06-11 17:27:36 -0700

    sets up ingredient page to display from URL params

--- recipe-box-app/src/components/FullRecipe.js
+++ recipe-box-app/src/components/FullRecipe.js
@@ -3,19 +3,19 @@ import { connect } from 'react-redux'
 import IngredientsContainer from '../containers/IngredientsContainer'
 
 class FullRecipe extends Component {
-    
-    renderRecipe = () => {
 
-        if (!!this.props.recipe) {
+    renderRecipe = () => {
+        let recipe = this.props.recipes.find(recipe => recipe.id === this.props.match.params.recipeId)
+        if (recipe) {
             return (
                 <div>
-                <p>full recipe</p>
-                <img src={this.props.recipe.image} alt="Image not Found"/>
-                <h3>{this.props.recipe.title}</h3>
+                <br/>
+                <img src={recipe.photo} alt="Image not Found" height="500px"/>
+                <h3>{recipe.title}</h3>
                 <h4>Instructions: </h4>
-                <p>{this.props.recipe.instructions}</p>
+                <p>{recipe.instructions}</p>
                 <h4>Ingredients: </h4>
-                <IngredientsContainer ingredients = {this.props.ingredients}/> 
+                <IngredientsContainer ingredients = {this.props.ingredients.filter(ingredient => ingredient.recipeId === this.props.match.params.recipeId)}/> 
                 </div>
             )
         } else {
@@ -41,8 +41,8 @@ class FullRecipe extends Component {
 
 const mapStateToProps = (state, ownProps) => {
     return { 
-        recipe: state.recipes.recipes.find(recipe => recipe.id === ownProps.match.params.recipeID), 
-        ingredients: state.ingredients.filter(ingredient => ingredient.recipeId === ownProps.match.params.recipeID)
+        recipes: state.recipes.recipes, 
+        ingredients: state.ingredients
     }
 }
 export default connect(mapStateToProps)(FullRecipe)
